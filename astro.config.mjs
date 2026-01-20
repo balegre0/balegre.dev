8bf8a7bc39e15a9d38087d233cb43656d7479274
balegre0 <hi@balegre.dev>
2026-01-20 18:53:13 -0400

    init: sv replace to astro

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://balegre.dev",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
